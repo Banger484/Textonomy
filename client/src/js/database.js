44619fe78eaba2bbbe6278a8c96cbf12760ace1b
Bret Banger <Banger484@gmail.com>
2022-05-18 07:23:16 -0500

    filling out database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textonomyDB = await openDB('textonomy', 1)
+  const tx = textonomyDB.transaction('textonomy', 'readwrite')
+  const store = tx.objectStore('textonomy')
+  const request =store.add(content)
+
+  const result = await request;
+
+  console.log('🚀 - data save successful!', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const textonomyDB = await openDB('textonomy', 1);
+  const tx = textonomyDB.transaction('textonomy', 'readonly');
+  const store = tx.objectStore('textonomy')
+  const request = store.getAll()
+
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
